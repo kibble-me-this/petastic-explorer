2164895204a19d47a3afd3d75221c97ce37c56f6
Carlos Herrera <carlos@petastic.com>
2023-10-31 23:35:01 -0400

    Added auto scrolling on chat area

--- src/sections/chat/hooks/use-messages-scroll.js
+++ src/sections/chat/hooks/use-messages-scroll.js
@@ -1,9 +1,54 @@
-import { useCallback, useEffect, useRef } from 'react';
-
-// ----------------------------------------------------------------------
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function useMessagesScroll(messages) {
   const messagesEndRef = useRef(null);
+  const [websiteScrollTop, setWebsiteScrollTop] = useState(0);
+  const scrollSpeedFactor = 0.5; // Adjust the scroll speed factor as needed
+
+  const scrollMessagesToStart = useCallback(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollTop = 0; // Scroll to the top
+    }
+  }, []);
+
+  useEffect(() => {
+    // Delay the scroll to start by 1 second
+    const delay = setTimeout(() => {
+      scrollMessagesToStart(); // Scroll to the start after 1 second
+    }, 1000);
+
+    return () => {
+      clearTimeout(delay);
+    };
+  }, [scrollMessagesToStart]);
+
+  const handleWebsiteScroll = useCallback(() => {
+    setWebsiteScrollTop(window.pageYOffset);
+  }, []);
+
+  useEffect(() => {
+    handleWebsiteScroll(); // Set the initial scroll position
+  }, [handleWebsiteScroll]);
+
+  // Synchronize chat message list's scroll position with website's scroll position
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      // Adjust the scroll speed using scrollSpeedFactor
+      messagesEndRef.current.scrollTop = websiteScrollTop * scrollSpeedFactor;
+    }
+  }, [websiteScrollTop]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      handleWebsiteScroll(); // Update the website scroll position
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleWebsiteScroll]);
 
   const scrollMessagesToBottom = useCallback(() => {
     if (!messages) {
@@ -19,13 +64,9 @@ export default function useMessagesScroll(messages) {
     }
   }, [messages]);
 
-  useEffect(
-    () => {
-      scrollMessagesToBottom();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [messages]
-  );
+  // useEffect(() => {
+  //   scrollMessagesToBottom();
+  // }, [messages]);
 
   return {
     messagesEndRef,
