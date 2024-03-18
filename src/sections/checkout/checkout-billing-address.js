da45e1a3a312ca39d24f18e6902b7fa992949450
Carlos Herrera <carlos@petastic.com>
2024-03-18 17:31:26 -0400

    Updated addresses to pull form fosters entries in document

--- src/sections/checkout/checkout-billing-address.js
+++ src/sections/checkout/checkout-billing-address.js
@@ -6,6 +6,8 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { _addressBooks } from 'src/_mock';
 // hooks
 import { useBoolean } from 'src/hooks/use-boolean';
+// api
+import { useGetFosters } from 'src/api/fosters';
 // components
 import Iconify from 'src/components/iconify';
 //
@@ -15,59 +17,61 @@ import { AddressNewForm, AddressItem } from '../address';
 
 // ----------------------------------------------------------------------
 
-const _addresses = [
-  {
-    name: 'Carlos Herrera',
-    phoneNumber: '310-880-8673',
-    address: '2900 NE 7Th Ave Unit 2006',
-    city: 'Miami',
-    state: 'HI',
-    country: 'US',
-    zip: '33137',
-    fullAddress: '2900 NE 7Th Ave Unit 2006, Miami, FL, US, 33137',
-    addressType: 'HQ',
-    primary: true,
-  },
-  {
-    name: 'Lucian Obrien',
-    phoneNumber: '904-966-2836',
-    address: '1147 Rohan Drive Suite 819',
-    city: 'Burlington',
-    state: 'VT',
-    country: 'US',
-    zip: '82021',
-    fullAddress: '1147 Rohan Drive Suite 819, Burlington, VT, US, 82021',
-    addressType: 'Foster',
-    primary: false,
-  },
-  {
-    name: 'Deja Brady',
-    phoneNumber: '399-757-9909',
-    address: '18605 Thompson Circle Apt. 086',
-    city: 'Idaho Falls',
-    state: 'WV',
-    country: 'US',
-    zip: '50337',
-    fullAddress: '18605 Thompson Circle Apt. 086, Idaho Falls, WV, US, 50337',
-    addressType: 'Foster',
-    primary: false,
-  },
-  {
-    name: 'Harrison Stein',
-    phoneNumber: '692-767-2903',
-    address: '110 Lamar Station Apt. 730',
-    city: 'Hagerstown',
-    state: 'OK',
-    country: 'US',
-    zip: '49808',
-    fullAddress: '110 Lamar Station Apt. 730, Hagerstown, OK, US, 49808',
-    addressType: 'Foster',
-    primary: false,
-  },
-];
+// const _addresses = [
+//   {
+//     name: 'Carlos Herrera',
+//     phoneNumber: '310-880-8673',
+//     address: '2900 NE 7Th Ave Unit 2006',
+//     city: 'Miami',
+//     state: 'FL',
+//     country: 'US',
+//     zip: '33137',
+//     fullAddress: '2900 NE 7Th Ave Unit 2006, Miami, FL, US, 33137',
+//     addressType: 'HQ',
+//     primary: true,
+//   },
+//   {
+//     name: 'Lucian Obrien',
+//     phoneNumber: '904-966-2836',
+//     address: '1147 Rohan Drive Suite 819',
+//     city: 'Burlington',
+//     state: 'VT',
+//     country: 'US',
+//     zip: '82021',
+//     fullAddress: '1147 Rohan Drive Suite 819, Burlington, VT, US, 82021',
+//     addressType: 'Foster',
+//     primary: false,
+//   },
+//   {
+//     name: 'Deja Brady',
+//     phoneNumber: '399-757-9909',
+//     address: '18605 Thompson Circle Apt. 086',
+//     city: 'Idaho Falls',
+//     state: 'WV',
+//     country: 'US',
+//     zip: '50337',
+//     fullAddress: '18605 Thompson Circle Apt. 086, Idaho Falls, WV, US, 50337',
+//     addressType: 'Foster',
+//     primary: false,
+//   },
+//   {
+//     name: 'Harrison Stein',
+//     phoneNumber: '692-767-2903',
+//     address: '110 Lamar Station Apt. 730',
+//     city: 'Hagerstown',
+//     state: 'OK',
+//     country: 'US',
+//     zip: '49808',
+//     fullAddress: '110 Lamar Station Apt. 730, Hagerstown, OK, US, 49808',
+//     addressType: 'Foster',
+//     primary: false,
+//   },
+// ];
 
 export default function CheckoutBillingAddress() {
   const checkout = useCheckoutContext();
+
+  const { fosters, isLoading } = useGetFosters("5ee83180fb01683673939629");
 
   const addressForm = useBoolean();
 
@@ -75,7 +79,7 @@ export default function CheckoutBillingAddress() {
     <>
       <Grid container spacing={3}>
         <Grid xs={12} md={8}>
-          {_addresses.slice(0, 4).map((address) => (
+          {fosters.slice(0, 10).map((address) => (
             <AddressItem
               key={address.id}
               address={address}
